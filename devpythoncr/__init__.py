3bf664536ea27f7f759fba439375fbb5fe0ddb69
resendecristovao <121773162+resendecristovao@users.noreply.github.com>
2023-05-18 10:46:34 -0300

    Update __init__.py

--- devpythoncr/__init__.py
+++ devpythoncr/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -10,10 +11,22 @@ app.config['SECRET_KEY'] = '27474225c680f8414d45600e3f7eedc1'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
+
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from devpythoncr import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("BD criado.")
+else:
+    print("BD existente.")
 
 from devpythoncr import routes
