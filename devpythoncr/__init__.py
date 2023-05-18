4ba54022dd08fc3590cb83cdebe28455f722af19
resendecristovao <121773162+resendecristovao@users.noreply.github.com>
2023-05-17 22:21:13 -0300

    Update __init__.py

--- devpythoncr/__init__.py
+++ devpythoncr/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '27474225c680f8414d45600e3f7eedc1'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
