c9ac16adc8dc4aefdf1350ab2d27046cb3412875
resendecristovao <121773162+resendecristovao@users.noreply.github.com>
2023-05-19 09:21:48 -0300

    Update __init__.py

--- devpythoncr/__init__.py
+++ devpythoncr/__init__.py
@@ -18,16 +18,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-from devpythoncr import models
-engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector = sqlalchemy.inspect(engine)
-print(inspector.get_table_names())
-if not inspector.has_table("Usuarios"):
+if not database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
     with app.app_context():
         database.drop_all()
         database.create_all()
         print("BD criado.")
-        print(inspector.get_table_names())
 else:
     print("BD existente.")
 
